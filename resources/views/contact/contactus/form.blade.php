e8f316530a9df3d638d9320a7d492c19f31b8caf
unknown <danie_000@EGALP-DCIO-005.egainternal.go.tz>
2015-12-09 20:32:00 +0300

    contact form fix

--- resources/views/contact/contactus/form.blade.php
+++ resources/views/contact/contactus/form.blade.php
@@ -1,16 +1,21 @@
 
+    <style>
+    input{
+        width: 98.5% !important;
+    }
+    </style>
+
+
       {!!  Form::open(array('route' => 'contact.send-mail')) !!}
 
       <p>
           {!! Form::text('name',null,['class'=>'','placeholder'=>'Enter Name']) !!}
           <span class='error'>{{ $errors->first('name') }}</span>
-          
-
-          {!! Form::text('email',null,['class'=>'','placeholder'=>'Enter your email']) !!}
-          <span class='error'>{{ $errors->first('email') }}</span>
-
 
       </p>
+
+      <p>          {!! Form::text('email',null,['class'=>'','placeholder'=>'Enter your email']) !!}
+          <span class='error'>{{ $errors->first('email') }}</span></p>
       <p>
           {!! Form::text('subject',null,['class'=>'','placeholder'=>'Enter subject']) !!}
           <span class='error'>{{ $errors->first('subject') }}</span>
@@ -22,7 +27,7 @@
 
       </p>
       <p>
-        <button type="submit"  class="btn btn-default"  >{{trans('messages.lbl_send')}}</button>
+        <button type="submit"  class="btn btn-default"    style="border-radius: 2px">{{trans('messages.lbl_send')}}</button>
       </p>
     {!! Form::close() !!}
 @section('js')
